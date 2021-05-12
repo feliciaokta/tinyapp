b9f12ac9c51d705d6a6bc235179c40f9ed7814c6
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-11 19:18:23 -0700

    added cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
